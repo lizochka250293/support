90b8cd58d76ef11cc01e1602947040f8644a6adc
vera <lerika250293@mail.ru>
2022-10-25 15:22:55 +0300

    add host

--- chat_support/services.py
+++ chat_support/services.py
@@ -1,16 +1,15 @@
 import requests
-
-
+from .token import api_token, channel_id
+API = api_token
+HOST = 'https://retail-support.rendez-vous.ru'
+CHANNEL = channel_id
 def send_telegram(text: str, number: str):
     """Отправка в тг"""
-    host = ''
-    api_token = "5440252830:AAG15UZiZ1v_EaclQ85YXRKBc86DNN40UCo"
     url = "https://api.telegram.org/bot"
-    channel_id = -1001658312828
-    url += api_token
+    url += API
     method = url + "/sendMessage"
-    to_send = f'{text}, http://{host}/chat_room/{number}/'
+    to_send = f'{text}, {HOST}/chat_room/{number}/'
     r = requests.post(method, data={
-        "chat_id": channel_id,
+        "chat_id": CHANNEL,
         "text": to_send
     })
